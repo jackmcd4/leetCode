9fb2a3add55f2f28214aa1aba479c0caa2c444da
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-08-24 13:51:00 -0700

    adds isUgly and beginnings of minSubArray

---
+++ minSubArray.js
@@ -0,0 +1,11 @@
+var minSubArray = function(n, s){
+var greatest = Math.max.apply(null, n);
+var ind = n.indexOf(greatest);
+    if(greatest >= s){
+        return [greatest];
+    } else{
+    
+    }
+}
+
+minSubArray([2, 3, 1, 2, 4, 3], 7)
